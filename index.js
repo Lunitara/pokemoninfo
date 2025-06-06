9c867f7d2c5d61df7a50cf4528d687b16d1773c2
unknown <fourh027@gmail.com>
2025-06-06 11:38:12 -0600

    started the javascript code

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-const getpokinfo = document.getElementById("getpokinfo")
+const getinfobutton = document.getElementById("getpokinfo")
+import TCGdex from '@tcgdex/sdk'
+const infoinput = document.getElementById("infoinput")
+
+getinfobutton.addEventListener("click", function () {
+    const input = document.createElement("input");
+    input.type = "text";
+    input.placeholder = `Pokemon Name ${i + 1}`;
+    myDiv.appendChild(input);
+})
+
+
